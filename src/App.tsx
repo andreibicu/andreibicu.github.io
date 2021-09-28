706988bd57fbade540e73cd69c8568a635ef0222
abicu <andrei.bicu.23@gmail.com>
2021-09-28 15:21:20 +0200

    add project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ overflow: 'hidden' }}>
+      <div
+        className="logo"
+        style={{
+          display: 'flex',
+          height: '100vh',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontFamily: 'sans-serif',
+          fontSize: 18,
+          letterSpacing: '0.025em',
+          color: 'green'
+        }}>
+        In progress ...
+      </div>
+    </div>
+  );
+}
+
+export default App;
